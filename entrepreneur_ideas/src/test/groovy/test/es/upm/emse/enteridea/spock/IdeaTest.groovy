86f544f4865fbf82022a294ad401949675ceabbd
ottoabreu <ottoabreu@gmail.com>
2013-12-23 14:04:26 +0100

    again...

--- entrepreneur_ideas/src/test/groovy/test/es/upm/emse/enteridea/spock/IdeaTest.groovy
+++ entrepreneur_ideas/src/test/groovy/test/es/upm/emse/enteridea/spock/IdeaTest.groovy
@@ -5,10 +5,15 @@ import static org.junit.Assert.fail;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import es.upm.emse.enteridea.business.IdeaManager;
 import es.upm.emse.enteridea.business.TopicManager;
 import es.upm.emse.enteridea.business.UserManager;
 import es.upm.emse.enteridea.business.exception.BusinessException;
+import es.upm.emse.enteridea.persistence.PersistenceManager;
 import es.upm.emse.enteridea.persistence.entity.Idea;
 import es.upm.emse.enteridea.persistence.entity.IdeaComment;
 
@@ -51,6 +56,20 @@ class IdeaTest  extends spock.lang.Specification {
 		ideaManager = null;
 		topicManager = null;
 		userManager = null;
+		cleanDB();
+	}
+	
+	def cleanDB(){
+		try {
+			Session session = PersistenceManager.getSessionFactory().openSession();
+			Transaction tx = session.beginTransaction();
+			session.createQuery("DELETE FROM User WHERE lastName ='"+TEST_USER_LAST_NAME+"'").executeUpdate();
+			session.createQuery("DELETE FROM Topic WHERE topicDescription ='"+TEST_TOPIC_NAME+"'").executeUpdate();
+			tx.commit();
+		} catch (HibernateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	def "Create test idea and verify its creation in the database"(){
